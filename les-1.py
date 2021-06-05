4993f71637fd2304c0e673d686e017345936b1ac
Ilya Kamardin <2stepsfrombug@gmail.com>
2021-06-05 15:27:29 +0300

    list and  tuple

--- les-1.py
+++ les-1.py
@@ -1,14 +1,16 @@
 # comments we can use 2 type of comments first is one line => #
 ''' second we can use the multiline comment'''
 # Arithmetic operators in Python
+# a//b  this is int devide
 
-# how to
+a = 5.5
 
+list_example = [10, a, "yosi"]
+print(type(list_example))
+list_example.append(68.3)
 
+tuple_example = (10, a, "yosi")
+print(type(tuple_example))
 
-
-
-
-
-
-
+# the diference between list and tuple the tuple we can't change its help for define some value that we not need to chane
+# in the code for example if we need to define month of the year we not wont for some reason we change it
